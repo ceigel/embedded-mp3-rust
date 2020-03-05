7ce0748d5ec3f597b7b90d8e666691c614507279
Cristian Eigel <cristian.eigel@icloud.com>
2020-03-05 21:40:52 +0100

    Remove dependency on bindgen

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
-extern crate bindgen;
 extern crate cc;
-use std::env;
-use std::path::PathBuf;
+//use std::env;
+//use std::path::PathBuf;
 
 fn main() {
     let mut build = cc::Build::new();
@@ -10,7 +9,7 @@ fn main() {
         .file("ffi/minimp3.c")
         .compile("minimp3");
 
-    let bb = bindgen::builder()
+    /*    let bb = bindgen::builder()
         .header("ffi/bindgen.h")
         .ctypes_prefix("cty")
         .generate_comments(true)
@@ -19,14 +18,13 @@ fn main() {
         .clang_arg(format!("--target={}", env::var("HOST").unwrap()))
         .use_core();
 
-    // bindgen --output src/bindings.rs bindgen.h --rust-target 1.33 --no-derive-default --ctypes-prefix cty --generate functions,types,vars,methods,constructors,destructors --use-core -- -Iminimp3
     let bindings = bb.generate().expect("Unable to generate bindings");
 
-    //let out_path = PathBuf::from("src");
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_path = PathBuf::from("src");
+    //let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
     println!("cargo:rerun-if-changed=bindgen.h");
-    println!("cargo:rerun-if-changed=minimp3.c");
+    println!("cargo:rerun-if-changed=minimp3.c"); */
 }
